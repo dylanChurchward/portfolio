a26bfac29d801639b4cd366eafe687c97b8fd686
Dylan <dylanchurchward710@gmail.com>
2022-09-20 12:11:05 -0700

    fixes small issue where new entries to the score board weren't loading right away

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// dependencies: npm install pixi.js
 
 const Application = PIXI.Application;
 
@@ -134,6 +133,8 @@ async function putLeaderboard(thePlayer, theScore) {
     });
 
     const scores = await response.json();
+
+    updateLeaderboard();
 }
 
 // Retrieve list of top 8 entries (sorted by score) from the leader board database 
@@ -556,7 +557,7 @@ class Tile {
                     cornerAngle)
                 .endFill();
 
-            if (this.value == 6561) {
+            if (this.value == 27) {
                 gameFinished();
             }
         };
@@ -835,7 +836,9 @@ document.addEventListener('keydown', function (e) {
 // the game state. 
 function startGame() {
     if (scoreBoxes != null) {
-        putLeaderboard(scoreBoxes[0].getText(), totalScore);
+        putLeaderboard(scoreBoxes[0].getText(), totalScore); // add new entry to the leader board database 
+
+        // execute/get rid of existing tiles 
         for (var i = 0; i < board.length; i++) {
             for (var j = 0; j < board.length; j++) {
                 if (board[i][j] != null) {
@@ -843,20 +846,37 @@ function startGame() {
                 }
             }
         }
+
+         // Collect new score boxes 
+        scoreBoxes = [];
+
+    for (let i = 0; i < scoreCardCount; i++) {
+        var scoreBox = new ScoreBox(i);
+        scoreBoxes.push(scoreBox);
     }
 
-    scoreBoxes = [];
+    } else {
+        // Collect new score boxes 
+        scoreBoxes = [];
+        for (let i = 0; i < scoreCardCount; i++) {
+            var scoreBox = new ScoreBox(i);
+            scoreBoxes.push(scoreBox);
+        }
+        updateLeaderboard()
+    }
+
+    // scoreBoxes = [];
     board = [];
     canPlay = true;
     isPlayable = true; 
     gameOver = false;
     totalScore = 0;
 
-    // Draw score board boxes on the score board 
-    for (let i = 0; i < scoreCardCount; i++) {
-        var scoreBox = new ScoreBox(i);
-        scoreBoxes.push(scoreBox);
-    }
+    // // Collect new score boxes 
+    // for (let i = 0; i < scoreCardCount; i++) {
+    //     var scoreBox = new ScoreBox(i);
+    //     scoreBoxes.push(scoreBox);
+    // }
 
     scoreBoxes[0].setText("Score: ")
     scoreBoxes[0].setScore(0)
@@ -865,8 +885,9 @@ function startGame() {
     scoreBoxes[1].setScore()
     scoreBoxes[1].setColor(colorMap.get(2187))
 
-    updateLeaderboard()
+    // updateLeaderboard()
 
+    // Create a 2d array of slots for tiles, representing the game board 
     for (var i = 0; i < tileCount; i++) {
         board[i] = [];
         for (var j = 0; j < tileCount; j++) {
