a92ee5e9e146af80bf8f953ce8fd990057af5d47
Dylan <dylanchurchward710@gmail.com>
2022-11-02 17:12:17 -0700

    created a solid start to my geology class project

---
+++ geo.js
@@ -0,0 +1,14 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 30,
+}).addTo(map);
+
+var map_2 = L.map('map_2').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 30,
+}).addTo(map_2);
+
+var map_3 = L.map('map_3').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 30,
+}).addTo(map_3);
